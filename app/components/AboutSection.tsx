541a38a0a3b13b4fd3190ded27a0255339d73639
Nicolay B. Rennemo <rennemo@proton.me>
2025-10-01 17:48:00 +0200

    Added animated background to about sections, and gave navbar more padding

--- app/components/AboutSection.tsx
+++ app/components/AboutSection.tsx
@@ -1,6 +1,5 @@
 import { Icon } from "@iconify/react";
-import { ReactNode } from "react"
-
+import { ReactNode } from "react";
 
 interface AboutSectionProps {
   icon: string;
@@ -10,7 +9,7 @@ interface AboutSectionProps {
 
 export const AboutSection = ({ icon, title, children }: AboutSectionProps) => {
   return (
-    <div className="flex m-3">
+    <div className="rounded-md m-1 p-2 flex bg-no-repeat hover:bg-gradient-to-r from-blue-900 to-transparent bg-[length:0_100%] hover:bg-[length:100%_100%] transition-all duration-400">
       <div>
         <Icon icon={icon} className="size-15" />
       </div>
@@ -20,4 +19,4 @@ export const AboutSection = ({ icon, title, children }: AboutSectionProps) => {
       </div>
     </div>
   );
-}
+};
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -22,7 +22,7 @@ const navLinks = [
 
 export const Navbar = () => {
   return (
-    <div className="flex p-4 w-full">
+    <div className="flex p-10 w-full">
       <p className="grow font-bold text-4xl">John Doe</p>
       <NavLinks links={navLinks} />
       <MobNavLinks links={navLinks} />
