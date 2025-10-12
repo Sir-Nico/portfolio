780bd8d5e3672fea823e63614436f2cf92bf4a4d
Nicolay B. Rennemo <rennemo@proton.me>
2025-10-12 23:15:14 +0200

    Added an arrow that you can click to get to the about section from the hero section without scrolling

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -9,16 +9,22 @@ export const Hero = () => {
   const goToContact = () => {
     window.location.href = "#contact";
   };
+
   const goToLinkedIn = () => {
     window.location.href = "https://www.linkedin.com/in/nicolay-rennemo/";
   };
+
   const goToGitHub = () => {
     window.location.href = "https://github.com/Sir-Nico";
   };
 
+  const goToAbout = () => {
+    window.location.href = "#about";
+  };
+
   return (
     <div className="flex max-sm:flex-col w-4/5 items-center p-5 m-10 h-screen">
-      <div className="grow space-y-5">
+      <div className="grow space-y-5 max-sm:pt-45">
         <p className="text-6xl font-bold">
           Hi, I'm{" "}
           <span className="text-6xl font-roboto_m font-bold text-accent animate-bounce">
@@ -30,10 +36,10 @@ export const Hero = () => {
         <div className="flex space-x-2">
           <Button onClick={goToContact}>Contact Me</Button>
           <Button onClick={goToLinkedIn}>
-            <Icon icon="mdi:linkedin" className="size-6"/>
+            <Icon icon="mdi:linkedin" className="size-6" />
           </Button>
           <Button onClick={goToGitHub}>
-            <Icon icon="mdi:github" className="size-6"/>
+            <Icon icon="mdi:github" className="size-6" />
           </Button>
         </div>
       </div>
@@ -45,6 +51,11 @@ export const Hero = () => {
           alt="An image of me"
           className="rounded-xl"
         />
+      </div>
+      <div className="absolute left-[50%] top-[81%]">
+        <Button onClick={goToAbout} className="bg-transparent hover:shadow-none hover:bg-transparent p-0 max-sm:hidden">
+          <Icon icon="tabler:arrow-down" className="size-8" />
+        </Button>
       </div>
     </div>
   );
