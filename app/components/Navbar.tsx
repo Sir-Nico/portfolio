d370ed8521aa53e15ad479af35ca797f8d4f82e6
Nicolay B. Rennemo <rennemo@proton.me>
2025-10-13 00:04:29 +0200

    Smooth scrolling for title link as well

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,6 +1,7 @@
+"use client"
+
 import NavLinks from "./NavLinks";
 import MobNavLinks from "./MobNavLinks";
-import Link from "next/link";
 
 const navLinks = [
   {
@@ -24,7 +25,12 @@ const navLinks = [
 export const Navbar = () => {
   return (
     <div className="flex p-10 w-full fixed top-0 z-50 bg-background">
-      <Link href="/" className="grow font-bold text-4xl text-accent hover:text-accent/80 hover:font-extrabold transition-all duration-200">Nicolay B.</Link>
+      <a
+        href="#"
+        className="grow font-bold text-4xl text-accent hover:text-accent/80 hover:font-extrabold transition-all duration-200"
+      >
+        Nicolay B.
+      </a>
       <NavLinks links={navLinks} />
       <MobNavLinks links={navLinks} />
     </div>
