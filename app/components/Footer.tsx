86b958db6e75a49f012ee2bc14bf85223bbf067c
Nicolay B. Rennemo <rennemo@proton.me>
2025-10-13 14:30:25 +0200

    Added a footer component

---
+++ app/components/Footer.tsx
@@ -0,0 +1,9 @@
+
+
+export const Footer = () => {
+    return (
+        <div id="contact" className="bg-secondary">
+            <span>© 2025 Nico. All rights reserved.</span>
+        </div>
+    );
+};
