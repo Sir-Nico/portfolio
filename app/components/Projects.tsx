7e0b61166f7469e98e55b3872b3b4bac8a5289d2
Nicolay B. Rennemo <rennemo@proton.me>
2025-10-15 18:08:46 +0200

    Added the project section (empty atm)

---
+++ app/components/Projects.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export const Projects = () => {
+  return (
+    <div
+      id="projects"
+      className="flex flex-col w-4/5 max-sm:w-full p-5 scroll-m-40"
+    >
+      <p className="text-4xl font-bold p-4 pl-0">Projects</p>
+    </div>
+  );
+};
